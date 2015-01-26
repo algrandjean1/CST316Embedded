26714f86a7573fec1b058331f12d0b6287a19e79
Bryan Duarte <bjduarte@asu.edu>
2015-01-26 16:51:32 -0700

    Created the repo file system. Added eclipse file structure, embeddedSystems, and Misc folders

---
+++ airAutomation/src/airUI/pkg/MainPage.java
@@ -0,0 +1,14 @@
+package airUI.pkg;
+
+public class MainPage {
+
+	public MainPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ airAutomation/src/airUI/pkg/Reports.java
@@ -0,0 +1,14 @@
+package airUI.pkg;
+
+public class Reports {
+
+	public Reports() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
