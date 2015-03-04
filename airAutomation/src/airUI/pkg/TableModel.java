773041b1ac6bb83c2984cba5f59522c437ce125b
Fatimah <falowa@asu.edu>
2015-03-03 19:18:47 -0700

    creating tableModel class to work with report page

--- airAutomation/src/airUI/pkg/TableModel.java
+++ airAutomation/src/airUI/pkg/TableModel.java
@@ -1,1 +1,51 @@
+package airUI.pkg;
 
+import javax.swing.table.AbstractTableModel;
+
+public class TableModel extends AbstractTableModel {
+    
+    
+    String[] colNamesDaily = {"Time", "O2",  "CO2", "Humidity","Temperature"};
+    String[] colNamesWeekly = {"Week", "O2",  "CO2", "Humidity","Temperature"};
+    String[] colNames = {"Month", "O2",  "CO2", "Humidity","Temperature"};
+    String[] colNamesYearly = {"Year", "O2",  "CO2", "Humidity","Temperature"};
+    
+    
+    Object[][] data = {
+    {"", "", "", " ", " "},
+    {"", "", "", "", ""},
+    
+    };
+    
+    
+	public int getColumnCount() {
+		return colNames.length;
+	}
+    
+	public int getRowCount() {
+		return data.length;
+	}
+    
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return data[rowIndex][columnIndex];
+        
+	}
+	public String getColumnName(int index) {
+        return colNames[index];
+    }
+	
+	public void setData(String[] colNames, Object[][] data){
+		this.colNames = colNames;
+		this.data = data;
+		
+		fireTableDataChanged();
+		fireTableStructureChanged();
+	}
+    
+	public void addAverage(Object[] row) {
+		data[data.length-1] = row;
+		
+	}
+	
+    
+}
